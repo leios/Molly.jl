9f418f61fae29b4843dc679a84455746bc323ebe
James Schloss <jrs.schloss@gmail.com>
2022-08-25 10:36:42 +0200

    making the precision test a standalone file

--- test/precision_tests.jl
+++ test/precision_tests.jl
@@ -4,13 +4,13 @@ end
 
 Parallel(; nl_only=false) = parallel(nl_only)
 
-@inline @inbounds function force(inter::Parallel,
-                                 dr,
-                                 coord_i,
-                                 coord_j,
-                                 atom_i,
-                                 atom_j,
-                                 boundary)
+@inbounds function Molly.force(inter::Parallel,
+                               dr,
+                               coord_i,
+                               coord_j,
+                               atom_i,
+                               atom_j,
+                               boundary)
     println(dr)
     return SVector((0.1, 0.1))
 end
